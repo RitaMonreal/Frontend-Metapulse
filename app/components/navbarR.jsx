d07b5be1ce21980090cc9aa1bc0a9548608b9c70
RitaMonreal <A326347@alumnos.uaslp.mx>
2024-02-26 12:13:03 -0600

    modificaciones navbar R

--- app/components/navbarR.jsx
+++ app/components/navbarR.jsx
@@ -6,13 +6,14 @@ const Navbarr = () => {
   return (
     <nav className="flex justify-between bg-navbarPurple py-6 px-4">
       <div className="logo"></div>
-      <div className="money">
-        <div id="icon_moneda"></div>
-        <div>50</div>
-      </div>
-      <div className="profile-icon">RM</div>
+      <div className="flex left-content">
+        <div id="icon_moneda" className="mr-2"></div>
+        <div className="mr-8 white-text" >50</div>
+     
+      <div className="profile-icon mr-8">RM</div>
       <div className="buttons">
-        <button>Sell</button>
+        <button className='bg-blue-700'>Sell</button>
+      </div>
       </div>
     </nav>
   );
