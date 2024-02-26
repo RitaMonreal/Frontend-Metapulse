66c58e740d23077d92823af70bc6b47374177d7e
RitaMonreal <A326347@alumnos.uaslp.mx>
2024-02-26 14:07:44 -0600

    se agrega el websiteview, aun no terminado

--- app/components/items.jsx
+++ app/components/items.jsx
@@ -1,9 +1,9 @@
 function Items({ items }){
     return(
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 gap-10">
             {items.map((item) => (
-                <div key={item.id} className="flex flex-col items-center bg-customColor">
-                    <img src={item.avatar} alt={item.email}/>
+                <div key={item.id} className="flex flex-col items-center bg-customColor " style={{ width: '312px', height: '220px' }}>
+                    <img src={item.avatar} alt={item.email} style={{ width: '288px', height: '140px', padding: '16px' }}/>
                     <h5>{item.first_name} {item.last_name}</h5>
                     <p>{item.email}</p>
                 </div>
