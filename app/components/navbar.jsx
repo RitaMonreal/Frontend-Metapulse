ed6fd0f3fa53332977d6b3d0074781b697fc31d5
RitaMonreal <A326347@alumnos.uaslp.mx>
2024-02-25 22:53:53 -0600

    modificaciones a la navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,15 +1,20 @@
-const navbar = () => {
+const Navbar = () => {
   return (
     <header>
-      <div className="flex bg-navbarPurple justify-center py-6">
-        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Metapulse
-          </span>
+      <div className="flex justify-between bg-navbarPurple py-6 px-4">
+      <button 
+          className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+        >
+         <img 
+            src="/images/letras_metapulse.png" 
+            alt="Imagen item" 
+            style={{ maxWidth: '120px' }} // Establecer un ancho máximo para la imagen, por ejemplo, 50px
+          />
         </button>
+        
       </div>
     </header>
   );
 };
 
-export default navbar;
+export default Navbar;
