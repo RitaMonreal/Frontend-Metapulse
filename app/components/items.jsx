7d903bb41eb87e91229cc6164057676bfa6a249e
RitaMonreal <A326347@alumnos.uaslp.mx>
2024-02-28 22:30:30 -0600

    pequeñitas modificaciones

--- app/components/items.jsx
+++ app/components/items.jsx
@@ -2,16 +2,16 @@
 import {useRouter} from 'next/navigation'
 
 function Items({ items }) {
-    const router = useRouter(); // <-- Asegúrate de llamar a la función useRouter
+    const router = useRouter(); 
 
     const handleClick = (itemId) => {
-        router.push(`/itemview/${itemId}`); // <-- Corregir la sintaxis
+        router.push(`/itemview/${itemId}`); 
     };
 
     return(
         <div className="grid grid-cols-4 gap-4">
             {items.map((item) => (
-                <div key={item.id} className="flex flex-col items-center bg-customColor" style={{ width: '312px', height: '220px' }} onClick={() => handleClick(item.id)}>
+                <div key={item.id} className="flex flex-col items-center bg-customColor  mt-1 rounded-sm overflow-y-auto" style={{ width: '312px', height: '220px' }} onClick={() => handleClick(item.id)}>
                     <img src={item.avatar} alt={item.email} style={{ width: '288px', height: '140px', padding: '16px' }}/>
                     <h5>{item.first_name} {item.last_name}</h5>
                     <p>{item.email}</p>
--- app/components/navbarR.jsx
+++ app/components/navbarR.jsx
@@ -1,18 +1,25 @@
+"use client"
+import {useRouter} from 'next/navigation'
 import React from 'react';
 import '../components/navbarR.css';
 
 
 const Navbarr = () => {
+
+  const router = useRouter()
+
   return (
     <nav className="flex justify-between bg-navbarPurple py-6 px-4">
-      <div className="logo"></div>
+      <div className="logo hover:bg-slate-500" onClick={()=> {
+        router.push(`/websiteview`)
+      }}> </div>
       <div className="flex left-content">
         <div id="icon_moneda" className="mr-2"></div>
         <div className="mr-8 white-text" >50</div>
      
       <div className="profile-icon mr-8">RM</div>
       <div className="buttons">
-        <button className='bg-blue-700'>Sell</button>
+        <button className="bg-blue-700 hover:bg-slate-500 mx-auto">Sell</button>
       </div>
       </div>
     </nav>
